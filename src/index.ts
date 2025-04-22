63de43009e11a47114c01f639f27756f266e1289
Preston Workstation <me@prestonarnold.uk>
2025-04-22 08:10:46 +0000

    Add entry point

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Core types
+export type {
+  FrameworkInfo,
+  UtilityLibrary,
+  PackageMetadataDetails,
+} from "./interfaces/MetadataExtractor";
+export type { FoundComponent } from "./interfaces/Finder";
+export type {
+  ComponentMetadata,
+  ExportType,
+} from "./interfaces/ComponentMetadata";
+export type { DetectionResult } from "./interfaces/Detector";
+
+// Interface types (for type checking only)
+export type { MetadataExtractor } from "./interfaces/MetadataExtractor";
+export type { Finder } from "./interfaces/Finder";
+export type { Formatter } from "./interfaces/Formatter";
+export type { Detector } from "./interfaces/Detector";
